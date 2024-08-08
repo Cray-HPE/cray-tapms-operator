3d39724ed1c288cb8eea219ba6c84a305caeaba8
Ryan Bak <rbak@cray.com>
2024-08-08 09:39:00 -0400

    CASMTRIAGE-7166: Fix status check when no hooks are defined

--- api/v1alpha3/utils.go
+++ api/v1alpha3/utils.go
@@ -2,7 +2,7 @@
  *
  *  MIT License
  *
- *  (C) Copyright 2022-2023 Hewlett Packard Enterprise Development LP
+ *  (C) Copyright 2022-2024 Hewlett Packard Enterprise Development LP
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a
  *  copy of this software and associated documentation files (the "Software"),
@@ -143,7 +143,7 @@ func TenantIsUpdated(tenant *Tenant) bool {
 		isUpdated = true
 	}
 
-	if !reflect.DeepEqual(tenant.Status.TenantHooks, tenant.Spec.TenantHooks) {
+	if (tenant.Spec.TenantHooks != nil) && (len(tenant.Spec.TenantHooks) > 0) && !reflect.DeepEqual(tenant.Status.TenantHooks, tenant.Spec.TenantHooks) {
 		isUpdated = true
 	}
 
