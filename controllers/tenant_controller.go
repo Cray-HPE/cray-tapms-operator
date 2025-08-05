7525e01b97f2fdd2fdd83f04c8c7eb4fbc58c295
Wisniewski <brett.wisniewski@hpe.com>
2025-08-05 16:55:06 -0600

    a test for always patching

--- controllers/tenant_controller.go
+++ controllers/tenant_controller.go
@@ -127,16 +127,16 @@ func (r *TenantReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		log.Info("Creating/updating Vault transit for: " + tenant.Spec.TenantName)
 		result, err = alphav3.CreateVaultTransit(ctx, log, tenant)
 
-		// This block checks if the `RequiresVaultKeyUpdate` field is true. If so, it resets the field to false
-		// by creating and applying a patch to the Kubernetes API, ensuring the change is persisted in the cluster.
-		if tenant.Spec.RequiresVaultKeyUpdate {
-			patch := client.MergeFrom(tenant.DeepCopy())
-			tenant.Spec.RequiresVaultKeyUpdate = false
+		// Resets the field to false by creating and applying a patch to the Kubernetes API
+		patch := client.MergeFrom(tenant.DeepCopy())
+		tenant.Spec.RequiresVaultKeyUpdate = false
 
-			if err := r.Client.Patch(ctx, tenant, patch); err != nil {
-				log.Error(err, "Failed to patch requiresVaultKeyUpdate field")
-				return ctrl.Result{}, err
-			}
+		if err := r.Client.Patch(ctx, tenant, patch); err != nil {
+			log.Error(err, "Failed to patch requiresVaultKeyUpdate field")
+			return ctrl.Result{}, err
+		}
+		// Prints the change only when true since the user set it this way
+		if tenant.Spec.RequiresVaultKeyUpdate {
 			log.Info(fmt.Sprintf("Reset requiresVaultKeyUpdate to false for tenant (%s)", tenant.Name))
 		}
 
